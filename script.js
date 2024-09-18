24c4b62801996328636196741162ad421e186347
Phantem717 <pradahsuherli@gmail.com>
2024-09-18 21:00:04 +0700

    C2

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 const HB = document.querySelector(".HB")
 const Navmenu = document.querySelector(".Navmenu")
 
 HB.addEventListener("click",() =>{
     HB.classList.toggle("active");
     HB.classList.toggle("active");
-=======
-const HB = document.querySelector(".HB")
-const Navmenu = document.querySelector(".Navmenu")
-
-HB.addEventListener("click",() =>{
-    HB.classList.toggle("active");
-    HB.classList.toggle("active");
->>>>>>> 836ba5a52e72b2a21107be1f52b6ccc6cb540f57
 })
