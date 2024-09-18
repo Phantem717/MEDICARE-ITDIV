b7453b4f9e2edb46f2ad7e79fcd83de19d422757
Phantem717 <pradahsuherli@gmail.com>
2024-09-18 20:52:11 +0700

    C1

---
+++ script.js
@@ -0,0 +1,7 @@
+const HB = document.querySelector(".HB")
+const Navmenu = document.querySelector(".Navmenu")
+
+HB.addEventListener("click",() =>{
+    HB.classList.toggle("active");
+    HB.classList.toggle("active");
+})
